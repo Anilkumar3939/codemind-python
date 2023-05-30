91b1d1dc34e04e3afddb73b88867c0ac5034ddcc
Anilkumar3939 <98636110+Anilkumar3939@users.noreply.github.com>
2023-05-30 19:28:10 +0530

    def fun1(n):     t=0     for i in range(2,n):         if(n%i==0):             t=1             return False     if t==0:         return True def fun(n):     x=n     y=n     while x!=0:         if fun1(x):             return x         x-=1         if fun1(y):             return y         y+=1
    for i in range(int(input())):
        n=int(input())
        print(fun(n))

---
+++ Nearest_Prime.py
@@ -0,0 +1,22 @@
+def fun1(n):
+    t=0
+    for i in range(2,n):
+        if(n%i==0):
+            t=1
+            return False
+    if t==0:
+        return True
+def fun(n):
+    x=n
+    y=n
+    while x!=0:
+        if fun1(x):
+            return x
+        x-=1
+        if fun1(y):
+            return y
+        y+=1
+
+for i in range(int(input())):
+    n=int(input())
+    print(fun(n))
