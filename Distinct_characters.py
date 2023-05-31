5654a3fd9e48e7cbf072d69493fc0305a58557eb
Anilkumar3939 <98636110+Anilkumar3939@users.noreply.github.com>
2023-05-31 19:18:25 +0530

    l=input().lower() n=l.split() l1=[] l2=[] for i in n:     for j in i:             l1.append(j) for i in l1:     if i not in l2 and l1.count(i)==1:         l2.append(i) l2.sort() print("".join(l2))

---
+++ Distinct_characters.py
@@ -0,0 +1,14 @@
+l=input().lower()
+n=l.split()
+l1=[]
+l2=[]
+for i in n:
+    for j in i:
+            l1.append(j)
+for i in l1:
+    if i not in l2 and l1.count(i)==1:
+        l2.append(i)
+l2.sort()
+print("".join(l2))
+    
+        
