6e517a0c274340af8776cfa8e18007e666fef6d3
Anilkumar3939 <98636110+Anilkumar3939@users.noreply.github.com>
2023-05-31 19:25:07 +0530

    l=input().lower() n=l.split() c=0 l1=[] l2=[] for i in n:     for j in i:             l1.append(j) for i in l1:     if i not in l2:         l2.append(i)         c+=1 print(c)

---
+++ Count_Distinct_characters.py
@@ -0,0 +1,13 @@
+l=input().lower()
+n=l.split()
+c=0
+l1=[]
+l2=[]
+for i in n:
+    for j in i:
+            l1.append(j)
+for i in l1:
+    if i not in l2:
+        l2.append(i)
+        c+=1
+print(c)
