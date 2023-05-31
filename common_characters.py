69b71a00a30b98d6038495c959181f9709c4a4fd
Anilkumar3939 <98636110+Anilkumar3939@users.noreply.github.com>
2023-05-31 20:14:41 +0530

    x=input().lower() y=input().lower() l11=[] l2=[] a=x.split() b=y.split() l1=[] for i in a:     for j in i:         l1.append(j) for i in b:     for j in i:         l2.append(j) for i in l1:     if i in l2:         l11.append(i) for i in l2:     if i in l1:         l11.append(i) l22=set(l11) l11=list(l22) l11.sort() if len(l11)!=0:     print("".join(l11)) else:     print("-1")

---
+++ common_characters.py
@@ -0,0 +1,26 @@
+x=input().lower()
+y=input().lower()
+l11=[]
+l2=[]
+a=x.split()
+b=y.split()
+l1=[]
+for i in a:
+    for j in i:
+        l1.append(j)
+for i in b:
+    for j in i:
+        l2.append(j)
+for i in l1:
+    if i in l2:
+        l11.append(i)
+for i in l2:
+    if i in l1:
+        l11.append(i)
+l22=set(l11)
+l11=list(l22)
+l11.sort()
+if len(l11)!=0:
+    print("".join(l11))
+else:
+    print("-1")
