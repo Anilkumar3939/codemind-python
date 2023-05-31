c1e4e61524c8e36cd06551a7a29bfdb085c8ac29
Anilkumar3939 <98636110+Anilkumar3939@users.noreply.github.com>
2023-05-31 20:38:51 +0530

    x=input().lower() a=x.split() l1=[] for j in a[0]:     t=0     for k in range(1,len(a)):         if j not in a[k]:             t=1     if t==0:         l1.append(j) if len(l1)==0:     print("-1") else:     print("".join(l1))

---
+++ Common_characters_-1.py
@@ -0,0 +1,14 @@
+x=input().lower()
+a=x.split()
+l1=[]
+for j in a[0]:
+    t=0
+    for k in range(1,len(a)):
+        if j not in a[k]:
+            t=1
+    if t==0:
+        l1.append(j)
+if len(l1)==0:
+    print("-1")
+else:
+    print("".join(l1))
