b8c9ba728d2c05160e75e550d6a0e175206f4e35
Anilkumar3939 <98636110+Anilkumar3939@users.noreply.github.com>
2023-06-01 19:46:08 +0530

    import math as m a=int(input()) b=int(input()) if a>b:     a,b=b,a c=0 def fun(i):     x=0     if i==1 or i==0:         return False     for j in range(2,int(m.sqrt(i))+1):         if i%j==0:             x=1             return False     if x==0:         return True for i in range(a,b+1):     if fun(i):         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,19 +1,20 @@
-import math
+import math as m
+a=int(input())
+b=int(input())
+if a>b:
+    a,b=b,a
+c=0
 def fun(i):
     x=0
-    if i==1:
+    if i==1 or i==0:
         return False
-    for j in range(2,int(math.sqrt(i))+1):
+    for j in range(2,int(m.sqrt(i))+1):
         if i%j==0:
             x=1
             return False
     if x==0:
         return True
-a=int(input())
-b=int(input())
-s=0
 for i in range(a,b+1):
     if fun(i):
-        s+=1
-print(s)
-        
+        c+=1
+print(c)
