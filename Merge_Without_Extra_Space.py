5d30bfc3ddb57cff0c7fa99bc4cec7ea56946928
Anilkumar3939 <98636110+Anilkumar3939@users.noreply.github.com>
2023-06-06 13:34:08 +0530

    n=int(input()) for i in range(n):     a,b=map(int,input().split())     l1=list(map(int,input().split()))     l2=list(map(int,input().split()))     l=[]     for i in l2:         l1.append(i)     l1.sort()     print(*l1)

---
+++ Merge_Without_Extra_Space.py
@@ -0,0 +1,11 @@
+n=int(input())
+for i in range(n):
+    a,b=map(int,input().split())
+    l1=list(map(int,input().split()))
+    l2=list(map(int,input().split()))
+    l=[]
+    for i in l2:
+        l1.append(i)
+    l1.sort()
+    print(*l1)
+    
