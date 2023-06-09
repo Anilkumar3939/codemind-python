03ff5d9940ce081eeeabaf081d7218b510193bd0
Anilkumar3939 <98636110+Anilkumar3939@users.noreply.github.com>
2023-06-09 15:41:32 +0530

    import math as m def fun(n):     if n==1 or n==0:         return False     for i in range(2,int(m.sqrt(n))+1):         if n%i==0:             return False     return True a=int(input()) b=int(input()) for i in range(a,b+1):     if fun(i):         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,23 +1,13 @@
-m=int(input())
-n=int(input())
-l=list()
-i=m
-count=0
-while i<=n:
-    j=2
-    count1=0
-    if(i==1):
-        count1=1
-    else:
-        while j<i:
-            if(i%j==0):
-                count1=1
-                break
-            else:
-                j=j+1
-    if(count1==0):
-        l.append(i)
-        count=count+1
-    i=i+1
-for i in range(0,count,1):
-    print(l[i])
+import math as m
+def fun(n):
+    if n==1 or n==0:
+        return False
+    for i in range(2,int(m.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+    if fun(i):
+        print(i)
